9528cd1d1be27f2ad4a3374e94502cdcb9b40347
Vulkain22 <clement.lebellec@orange.fr>
2018-05-15 15:11:05 +0200

    CLB11

---
+++ BO/Constantes.cs
@@ -0,0 +1,9 @@
+﻿namespace BO
+{
+    public static class Constantes
+    {
+        public const string Administrateur = "Administrateur";
+        public const string Organisateur = "Organisateur";
+        public const string Convive = "Convive";
+    }
+}
--- SolutionPrincipale/Startup.cs
+++ SolutionPrincipale/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using SolutionPrincipale.Models;
 
 [assembly: OwinStartupAttribute(typeof(SolutionPrincipale.Startup))]
 namespace SolutionPrincipale
@@ -9,6 +12,41 @@ namespace SolutionPrincipale
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            using (var context = new ApplicationDbContext())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    var roleManager = new RoleManager<IdentityRole>
+                    (new RoleStore<IdentityRole>(context));
+
+                    var userManager = new UserManager<ApplicationUser>
+                        (new UserStore<ApplicationUser>(context));
+
+                    if (!roleManager.RoleExists(UserRoles.Administrator))
+                    {
+                        roleManager.Create(new IdentityRole(UserRoles.Administrator));
+                        var admin = new ApplicationUser
+                        {
+                            UserName = "Admin@gmail.com",
+                            Email = "Admin@gmail.com"
+                        };
+
+                        var result = userManager.Create(admin, "Pa$$w0rd");
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRole(admin.Id, UserRoles.Administrator);
+
+                            if (!roleManager.RoleExists(UserRoles.Competitor))
+                                roleManager.Create(new IdentityRole(UserRoles.Competitor));
+
+                            if (!roleManager.RoleExists(UserRoles.Organizer))
+                                roleManager.Create(new IdentityRole(UserRoles.Organizer));
+
+                            transaction.Commit();
+                        }
+                    }
+                }
+            }
         }
     }
 }
