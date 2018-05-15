618899b8f9a9e965480039f64b96add42592c868
Vulkain22 <clement.lebellec@orange.fr>
2018-05-15 11:46:25 +0200

    CLB9

---
+++ DAL/EvenementRepository.cs
@@ -0,0 +1,30 @@
+﻿using BO;
+using System.Data.Entity;
+
+namespace DAL.Migrations
+{
+    class EvenementRepository<T> : GenericRepository<Evenement> where T : DbContext, IDbContext
+    {
+        public EvenementRepository(T context) : base(context)
+        {
+
+        }
+        public override void Update(Evenement evenement)
+        {
+            Evenement o = GetById(evenement.Id);
+            o.Id = evenement.Id;
+            o.Nom = evenement.Nom;
+            o.NbParticipants = evenement.NbParticipants;
+            o.Description = evenement.Description;
+            o.DebutEvenement = evenement.DebutEvenement;
+            o.FinEvenement = evenement.FinEvenement;
+            o.Adresse = evenement.Adresse;
+            o.Ville = evenement.Ville;
+            o.CodePostal = evenement.CodePostal;
+            o.Organisateur = evenement.Organisateur;
+            //List<Convive>
+            dbContext.SaveChanges();
+        }
+
+    }
+}
