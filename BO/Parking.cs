5b35dfac886733d70b09c1caeec53756aad5181e
Vulkain22 <clement.lebellec@orange.fr>
2018-05-16 10:23:50 +0200

    CLB12

--- BO/Parking.cs
+++ BO/Parking.cs
@@ -10,11 +10,16 @@ namespace BO
     {
         public int Id { get; set; }
         public string Nom { get; set; }
+        public string Statut { get; set; }
+        public int NbPlaces { get; set; }
+        public int NbPlacesLibres { get; set; }
+        public string GeometrieType { get; set; }
+        public List<double> Coordonnees { get; set; }
+        public string CrsType { get; set; }
+        public string CrsNom { get; set; }
         public string Adresse { get; set; }
         public string Ville { get; set; }
         public string CodePostal { get; set; }
-        public int NbPlaces { get; set; }
-        public int NbPlacesLibres { get; set; }
         public DateTime? HeureOuverture { get; set; }
         public DateTime? HeureFermeture { get; set; }
         public int PrixHoraire { get; set; }
