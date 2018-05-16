a885d6590ae0ab227a27aa6382ebe73c4f2708c6
drick35 <drick35@gmail.com>
2018-05-16 14:17:47 +0200

    datetime

---
+++ SolutionPrincipale/Extension/HtmlHelperExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SolutionPrincipale.Extension
+{
+    public static class HtmlHelperExtension
+    {
+        public static MvcHtmlString CustomerDate<TModel, TValue>(
+            this HtmlHelper<TModel> htmlHelper, 
+            Expression<Func<TModel, TValue>> expression, string name)
+        {
+            return MvcHtmlString.Create("<input id=\"single_cal\" name=\""+ name+"\" class=\"form-control\" placeholder=\"MM / DD / YYY\" type=\"text\" />");
+        }
+    }
+}
